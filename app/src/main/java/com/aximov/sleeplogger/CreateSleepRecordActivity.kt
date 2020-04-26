f05057b4787c20c1ed94fd764e83424903add4b6
aximov <d@alumni.tus.ac.jp>
2020-04-26 23:38:52 +0900

    Activity のみでの実装に変更

--- app/src/main/java/com/aximov/sleeplogger/CreateSleepRecordActivity.kt
+++ app/src/main/java/com/aximov/sleeplogger/CreateSleepRecordActivity.kt
@@ -1,12 +1,35 @@
 package com.aximov.sleeplogger
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.NumberPicker
+import kotlinx.android.synthetic.main.fragment_create_sleep_record.*
+import java.lang.NumberFormatException
 
 class CreateSleepRecordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_sleep_record)
+        val durationPicker = findViewById<NumberPicker>(R.id.durationPicker)
+        durationPicker.minValue = 0
+        durationPicker.maxValue = 24
+        durationPicker.value = 8
+
+        val button = findViewById<Button>(R.id.button_create_sleep_record)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            val duration = durationPicker.value
+            replyIntent.putExtra(EXTRA_REPLY, duration)
+            setResult(Activity.RESULT_OK, replyIntent)
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.aximov.sleeplogger.sleeplistsql.REPLY"
     }
 }
--- app/src/main/java/com/aximov/sleeplogger/MainActivity.kt
+++ app/src/main/java/com/aximov/sleeplogger/MainActivity.kt
@@ -3,30 +3,52 @@ package com.aximov.sleeplogger
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import android.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sleepViewModel: SleepViewModel
+    private val createSleepRecordActivityRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        toolbar.setupWithNavController(navController, appBarConfiguration)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview_sleep)
+        val adapter = SleepListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        sleepViewModel = ViewModelProvider(this).get(SleepViewModel::class.java)
+        sleepViewModel.allSleeps.observe(this, Observer { sleeps ->
+            // Update the cached copy of the sleeps in the adapter.
+            sleeps?.let { adapter.setSleeps(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton_first)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, CreateSleepRecordActivity::class.java)
+            startActivityForResult(intent, createSleepRecordActivityRequestCode)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,6 +64,25 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == createSleepRecordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getIntExtra(CreateSleepRecordActivity.EXTRA_REPLY, 0)?.let {
+                val id = 0 // set this 0 to auto-generate id
+                val sleep = Sleep(id, Date(), it)
+                sleepViewModel.insert(sleep)
+                Log.v("Saving", sleep.length.toString())
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Not saved",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
