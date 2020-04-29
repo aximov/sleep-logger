175a000ee76857d79983bb85af10417e35b1c283
aximov <d@alumni.tus.ac.jp>
2020-04-29 12:28:38 +0900

    誤って消した toolbar を復活

--- app/src/main/java/com/aximov/sleeplogger/MainActivity.kt
+++ app/src/main/java/com/aximov/sleeplogger/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         val adapter = SleepListAdapter(this)
         val layoutManager = LinearLayoutManager(this)
